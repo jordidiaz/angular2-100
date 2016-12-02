934ac724b2a488bedc8388e53f0376a9bc50ca22
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2016-12-02 17:13:47 +0100

    First component.

---
+++ app/hello-plain.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hello-plain',
+    template: '<h1>Hello Plain Concepts Bilbao!</h1>'
+})
+export class HelloPlainComponent {
+
+}
--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,24 @@
-// 1- Componente raíz de la app.
 import {Component} from "@angular/core";
-@Component({
-    selector:'pc-app', // En que etiqueta de nuestro html bootstrapeará la app.
-    template: `<h1>Hello Plain!</h1>` // Plantilla inline.
-})
-export class HelloPlain {
-}
-
-// 2- Módulo raíz: TLDR Los módulos de Angular agrupan funcionalidad .
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+
+import { HelloPlainComponent } from './hello-plain'; // Nuestro componente.
+
+@Component({
+    selector:'pc-app',
+    template: `<hello-plain></hello-plain>` // ¿y ese tag?
+})
+export class App { // Ahora se llama App.
+}
+
 @NgModule({
-    declarations: [HelloPlain], // Componentes (directivas y pipes) que contiene este módulo.
-    imports: [BrowserModule], // Módulo que contiene las directivas básicas de Angular (ngIf, ngFor).
-    bootstrap: [HelloPlain] // Que componente bootstrapeará este módulo. (Sólo se usa en el módulo raíz).
+    declarations: [App, HelloPlainComponent], // Hay que registrar el componente en el módulo.
+    imports: [BrowserModule],
+    bootstrap: [App]
 })
 export class AppModule {
 
 }
 
-// 3- Bootstrap!
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 platformBrowserDynamic().bootstrapModule(AppModule);

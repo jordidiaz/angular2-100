e166bc25be6d8bf8163cdf44e861bd6064ede023
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2016-12-06 11:04:32 +0100

    Events and Zones 2

--- app/events-and-zones-component/events-and-zones.component.ts
+++ app/events-and-zones-component/events-and-zones.component.ts
@@ -5,8 +5,13 @@ declare const module: any;
 @Component({
     moduleId: module.id,
     selector: 'events-and-zones',
-    templateUrl: 'events-and-zones.template.html'
+    templateUrl: 'events-and-zones.template.html',
+    styleUrls: ['events-and-zones.style.css']
 })
 export class EventsAndZonesComponent {
+
+    onClick() {
+        
+    }
 
 }
